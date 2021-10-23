db82c6928c2e6ed15b613c66af26f30fde5cbdb7
oriolbaguena <88624743+oriolbaguena@users.noreply.github.com>
2021-10-23 10:37:41 +0200

    Add files via upload

--- Odd_or_Even.py
+++ Odd_or_Even.py
@@ -1,65 +1,61 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[13]:
 
 
 import random
 
 class Game:
-    
-    def __init__(self, cpu_num, user_selection):
+
+    def __init__(self):
         self.cpu_num = random.randint(1,6)
         self.user_selection = ["Odd", "Even"]
 
-    def cpu_choice(self, cpu_choice):
+    def Cpu_choice(self):
         self.cpu_choice = random.randint(1,6)
-        print("The computer's choice is",choice)
+        print("The computer's choice is",self.cpu_choice)
         return self.cpu_choice
 
-    def user_choice(self, user_choice):
+    def User_choice(self):
         while(True):
             self.user_choice = input("Odd or Even? ")
-            if choice in sself.user_selection:
-                print("Your choice is",choice)
+            if self.user_choice in self.user_selection:
+                print("Your choice is",self.user_choice)
                 return self.user_choice
             else:
-                print("You did not write your answer correctly, please try again.")            
+                print("You did not write your answer correctly, please try again.")
 
-    def round_result(self):
-        self.num_rounds = 0
-        self.user_lives = 10
+    def round_result(self, game):
+        num_rounds = 0
+        user_lives = 10
         while num_rounds < 8 and user_lives > 0:
-                user_pick = self.user_choice()
-                cpu_pick = self.cpu_choice()
+                user_pick = game.User_choice()
+                cpu_pick = game.Cpu_choice()
                 if (cpu_pick % 2 == 0) and (user_pick == "Even"):
-                        print (f'You have {self.user_lives + self.cpu_pick} lives left')
-                        self.num_rounds += 1
-                        self.user_lives = user_lives + cpu_pick
-                        return self.num_rounds
-                        return self.user_lives
+                        print (f'You have {user_lives + cpu_pick} lives left')
+                        num_rounds += 1
+                        user_lives = user_lives + cpu_pick
                 if (cpu_pick % 2 == 0) and (user_pick == "Odd"):
-                        print (f'You have {self.user_lives - self.cpu_pick} lives left')
-                        self.num_rounds += 1
-                        self.user_lives = user_lives - cpu_pick
-                        return self.num_rounds
-                        return self.user_lives
+                        print (f'You have {user_lives - cpu_pick} lives left')
+                        num_rounds += 1
+                        user_lives = user_lives - cpu_pick
                 if (cpu_pick % 2 != 0) and (user_pick == "Even"):
-                        print (f'You have {self.user_lives - self.cpu_pick} lives left')
-                        self.num_rounds += 1
-                        self.user_lives = user_lives - cpu_pick
-                        return self.num_rounds
-                        return self.user_lives
+                        print (f'You have {user_lives - cpu_pick} lives left')
+                        num_rounds += 1
+                        user_lives = user_lives - cpu_pick
                 if (cpu_pick % 2 != 0) and (user_pick == "Odd"):
-                        print (f'You have {self.user_lives + self.cpu_pick} lives left')
-                        self.num_rounds += 1
-                        self.user_lives = user_lives + cpu_pick
-                        return self.num_rounds
-                        return self.user_lives
+                        print (f'You have {user_lives + cpu_pick} lives left')
+                        num_rounds += 1
+                        user_lives = user_lives + cpu_pick
+
+def main():
 
 
-# In[ ]:
+    Odd_or_Even = Game()
+
+    Odd_or_Even.round_result(Odd_or_Even)
 
 
-
-
+if __name__ == '__main__':
+    main()
